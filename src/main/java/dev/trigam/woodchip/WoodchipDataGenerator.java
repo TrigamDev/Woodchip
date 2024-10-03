6a204abeab8e11201570a93226b4eb29843e6fa3
TrigamDev <trigambusiness@gmail.com>
2024-10-03 16:29:20 -0400

    Init

---
+++ src/main/java/dev/trigam/woodchip/WoodchipDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.trigam.woodchip;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WoodchipDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
